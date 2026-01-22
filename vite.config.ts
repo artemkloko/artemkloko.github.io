c40194e72f81c37748523e83ee74f551129d7ea3
artemkloko <artemkloko@gmail.com>
2026-01-22 18:56:07 +0200

    Add sitemap

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,20 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import tailwindcss from '@tailwindcss/vite'
 
+import Sitemap from 'vite-plugin-sitemap'
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     tailwindcss(),
+    Sitemap({
+      hostname: 'https://artemkloko.github.io',
+      dynamicRoutes: [
+        '/en',
+        '/de'
+      ],
+      readable: true
+    }),
   ],
 })
